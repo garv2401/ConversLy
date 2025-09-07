1c17c72472f9595e0395ddb85058cd5c3bbe8b8f
GARVIT <garvitkumar504@gmail.com>
2025-09-07 18:12:24 +0530

    Implement Popular Companions

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,46 @@
 import React from 'react'
-import {Button} from "@/components/ui/button";
-import Navbar from "@/components/Navbar"
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-4xl underline">Welcome to my saas app</h1>
-        <Button>Lets get started</Button>
+    <main>
+      <h1 className="text-4xl underline">Popular Companions</h1>
 
-    </div>
+        <section className="home-section">
+          <CompanionCard
+              id="123"
+              name="Neura the Brainy Explorer"
+              topic="Neural Networks of the Brain"
+              subject="science"
+              duration={45}
+              color="#ffda6e"
+          />
+          <CompanionCard
+              id="456"
+              name="Countsy the Number Wizard"
+              topic="Derivatives and Integrals"
+              subject="maths"
+              duration={30}
+              color="#efd0ff"
+          />
+          <CompanionCard
+              id="789"
+              name="Verba the Vocabulary Builder"
+              topic="English Literature"
+              subject="language"
+              duration={30}
+              color="#bde7ff"
+          />
+        </section>
+
+        <section className="home-section">
+          <CompanionsList/>
+          <CTA/>
+        </section>
+    </main>
   )
 }
 
